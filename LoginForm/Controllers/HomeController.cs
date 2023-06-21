ef4bd3b6abe5d43db34ddb9e05c804f23733be88
Dell <Dell@DESKTOP-CHTFR60>
2023-06-21 03:14:25 -0700

    Add project files.

---
+++ LoginForm/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using LoginForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(login log)
+        {
+            if(log.UserName != null && log.Password !=null )
+            {
+                return View();
+            }
+            return View("Index");
+        }
+    }
+}
